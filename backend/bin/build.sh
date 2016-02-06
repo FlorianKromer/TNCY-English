87a469ad42491d3119d952bb4645e3a13941529e
FlorianKromer <f.kromer54@gmail.com>
2016-02-06 15:09:53 +0100

    remove frontend dir

--- backend/bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-app/console assets:install web --symlink
-app/console assetic:dump
-app/console fos:js-routing:dump
-app/console cache:clear
--- backend/web/js/compiled/main_main_3.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-
-    $.material.init();
-    
-    $(document).ready(function() {
-	});
-
-
-})(jQuery);
